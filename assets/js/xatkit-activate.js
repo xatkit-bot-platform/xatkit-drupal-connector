3dc01bd4822b75c40a19e45ad1e7b84ee5f6721e
Joan Giner <giner.joan@gmail.com>
2021-02-17 13:37:24 +0100

    First POC done, widget now load correctly

---
+++ assets/js/xatkit-activate.js
@@ -0,0 +1,8 @@
+xatkit.renderXatkitWidget({
+  "server": "https://prod.xatkit.com/wordpress/custom/xat-bot/chat-handler",
+  "username": "Alice",
+  "storage": {
+    "autoClear": true
+  }
+})
+
--- src/Plugin/Block/XatkitBlock.php
+++ src/Plugin/Block/XatkitBlock.php
@@ -5,7 +5,7 @@ namespace Drupal\xatkit\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
- * Provides a 'testBlock' block.
+ * Provides a 'Xatkit' block.
  *
  * @Block(
  *  id = "xatkit_block",
@@ -18,15 +18,8 @@ class XatkitBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-
     return [
       '#theme' => 'xatkit',
-      '#variables' => '',
-      '#attached' => [
-        'library' => [
-          'xatkit/xatkit-assets',
-        ],
-      ],
     ];
   }
 
